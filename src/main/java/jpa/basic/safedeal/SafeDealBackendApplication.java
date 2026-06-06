b44a48b2427d9a37a2dbf1742d9274e83a79e2bb
CheatIsKey <silvull2t@naver.com>
2026-06-06 19:31:27 +0900

    init: Project Dependency Setting

---
+++ src/main/java/jpa/basic/safedeal/SafeDealBackendApplication.java
@@ -0,0 +1,13 @@
+package jpa.basic.safedeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeDealBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeDealBackendApplication.class, args);
+    }
+
+}
