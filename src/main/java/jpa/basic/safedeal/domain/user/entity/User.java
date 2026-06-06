c3759270721ea17edd4f5f123353a833184bebe4
CheatIsKey <silvull2t@naver.com>
2026-06-06 20:02:38 +0900

    CodeRabbit Test

---
+++ src/main/java/jpa/basic/safedeal/domain/user/entity/User.java
@@ -0,0 +1,19 @@
+package jpa.basic.safedeal.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+}
